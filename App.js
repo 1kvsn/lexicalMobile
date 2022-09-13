6daec99290b16d2c46b9a55c4a539b4bc02c03fb
Sasikant <nairs999@gmail.com>
2022-09-13 21:00:06 +0530

    App.js changes

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Button,
   FlatList,
   SafeAreaView,
   ScrollView,
@@ -9,30 +10,12 @@ import {
   TouchableHighlight,
   useColorScheme,
   View,
-} from 'react-native';
+} from "react-native";
 
 import WebView from 'react-native-webview';
+import NativeToWeb from './NativeToWeb';
+import WebToNative from './WebToNative';
 
-// const uri = [
-//   'https://573ktm.csb.app/',
-//   'https://573ktm.csb.app/',
-//   'https://573ktm.csb.app/',
-//   'https://573ktm.csb.app/',
-// ];
-
-// NOTE: This doesn't work. I think we can't pass dispatchEvent method inside a script and send inside web-view to execute there.
-function getInjectableJSMessage(message) {
-  return `
-    (function() {
-      document.dispatchEvent(new MessageEvent('message', {
-        data: ${JSON.stringify(message)}
-      }));
-    })();
-  `;
-}
-
-// Note: I wanted to emulate the feed and pass each JSON inside a web-view instance.
-// I wanted to dynamically pass the JSON from RN to Web-View
 const WebViewContainer = ({json}) => {
   const webviewRef = React.useRef();
 
@@ -55,61 +38,83 @@ const WebViewContainer = ({json}) => {
     true;
   `;
 
-  /**
-   * TODO: How to dynamically take JSON from Array of Posts and pass it into web-view ? NOTE: I can't figure out how to listen to this data in Web App.
-   * Only when I know I've received the data, I can take certain actions inside the web-view component for Lexical Editor.
-   * **/
-  const callThisFromEditorInsideWebView = `
-    window.ReactNativeWebView.postMessage('{"name":"thanos","place":["moon"]}')
-    true;
-  `;
+
+  // const injectSetJSON = `window.__kvsn.setJSON({"type":"setJSON","data":{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"This is first post of this feed.","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}})
+  // true;`;
+
+
+  // window.ReactNativeWebView.postMessage(JSON.stringify(window.__kvsn))
+  // window.postMessage('sasikant')
+  // window.__kvsn.exportJSON()
 
   // https://github.com/react-native-webview/react-native-webview/blob/master/docs/Guide.md#the-injectjavascript-method
-  React.useEffect(() => {
-    // NOTE: setTimeout was not added for any reason. We can remove this and the code inside will still work
-    setTimeout(() => {
-      // NOTE: This works fine. We call this method at our convenience and pass the JS. This might be of our use.
-      // webviewRef.current.injectJavaScript(runAtMyConvenience);
+  // React.useEffect(() => {
+  //   setTimeout(() => {
+  //     webviewRef.current?.injectJavaScript('window.__kvsn.setJSON()');
+  //   }, 2000)
+  // }, []);
 
-      webviewRef.current.injectJavaScript(callThisFromEditorInsideWebView);
+  // const jsCode = `
+  //   (function(){
+  //       window.__kvsn.setJSON()
+  //   })()`;
 
-      // Note: This commented code doesn't work
+  const jsCode = `
+    try {
+       window.__kvsn.setJSON()
+    } catch (e) {
+      window.ReactNativeWebView.postMessage(JSON.stringify({error: 'setJSON did not fire'}));
+      true;
+    }
+    true`;
 
-      // webviewRef.current.injectJavaScript(
-      //   getInjectableJSMessage({
-      //     name: 'Sasikant',
-      //     place: 'mooon',
-      //   }),
-      // );
-    }, 3000);
-  }, [callThisFromEditorInsideWebView]);
+  const _onMessage = event => {
+    const dataFromWebView = JSON.parse(event.nativeEvent.data);
+    console.log('_onMessage => dataFromWebView', dataFromWebView);
+  };
+
+  const onClick = () => {
+    console.log('clicked')
+    webviewRef.current?.injectJavaScript(jsCode);
+  };
+
+  const handleLoadEnd = () => {
+    webviewRef.current?.injectJavaScript(onLoadGetStore);
+
+  };
+
+  const onLoadGetStore = `
+    try {
+      function onLoadPageEnd() {
+        window.ReactNativeWebView.postMessage(JSON.stringify({success: true}));
+      }
+      window.onload = onLoadPageEnd()
+    } catch (e) {
+      window.ReactNativeWebView.postMessage(JSON.stringify({success: false}));
+      true;
+    }
+    true`;
 
   return (
-    <WebView
-      ref={ref => (webviewRef.current = ref)}
-      source={{uri: 'http://localhost:3001/'}}
-      onMessage={e => {
-        // Note: This works fine.
-        console.log(e.nativeEvent, '--- data  from Editor');
-      }}
-      style={{flex: 1}}
-      onLoadEnd={() => {
-        // NOTE: This works fine. However, we need to know when to fire a script which we pass this way.
-        // onLoadEnd might not be the best event on which we can run this kind of script. Not sure.
-        webviewRef.current.injectJavaScript(callThisFromEditorInsideWebView);
-      }}
+    <React.Fragment>
+      <Button onPress={onClick} title={'RN to WebView'} />
+      <WebView
+        ref={ref => (webviewRef.current = ref)}
+        source={{uri: 'http://localhost:3001/'}}
+        onMessage={_onMessage}
+        style={{flex: 1}}
+        onLoadEnd={handleLoadEnd}
+        javaScriptEnabled={true}
 
-      // Note: This works. This runs ONCE only. Might not be of our use.
-      // injectedJavaScript={runFirst}
-    />
+        // Note: This works. This runs ONCE only. Might not be of our use.
+        // injectedJavaScript={runFirst}
+      />
+    </React.Fragment>
+
   );
 };
 
 const App = () => {
-  // NOTE: I wanted to emulate the api response and render list of web-views and pass individual JSON inside each web-view component.
-  // I wanted to dynamically pass the JSON from RN to Web-View and render each web-view component with its own JSON.
-  // The raw JSON dummy json list is stored in a variable at the bottom of this file.
-  // Remember, it is a list of RAW JSON. It is not stringified.
   return (
     <SafeAreaView style={styles.container}>
       {DUMMY_FEED.map((json, index) => {
@@ -120,6 +125,9 @@ const App = () => {
           />
         );
       })}
+
+      {/*<NativeToWeb />*/}
+      {/*<WebToNative />*/}
     </SafeAreaView>
   );
 };
@@ -138,6 +146,39 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+const stringifiedJSON = JSON.stringify({
+  type: 'setJSON',
+  data: {
+    root: {
+      children: [
+        {
+          children: [
+            {
+              detail: 0,
+              format: 0,
+              mode: 'normal',
+              style: '',
+              text: 'This is first post of this feed.',
+              type: 'text',
+              version: 1,
+            },
+          ],
+          direction: 'ltr',
+          format: '',
+          indent: 0,
+          type: 'paragraph',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'root',
+      version: 1,
+    },
+  },
+});
 
 // each object represents a post JSON content
 const DUMMY_FEED = [
@@ -199,238 +240,238 @@ const DUMMY_FEED = [
       version: 1,
     },
   },
-  {
-    root: {
-      children: [
-        {
-          children: [
-            {
-              detail: 0,
-              format: 0,
-              mode: 'normal',
-              style: '',
-              text: 'This is third post of this feed.',
-              type: 'text',
-              version: 1,
-            },
-          ],
-          direction: 'ltr',
-          format: '',
-          indent: 0,
-          type: 'paragraph',
-          version: 1,
-        },
-      ],
-      direction: 'ltr',
-      format: '',
-      indent: 0,
-      type: 'root',
-      version: 1,
-    },
-  },
-  {
-    root: {
-      children: [
-        {
-          children: [
-            {
-              detail: 0,
-              format: 0,
-              mode: 'normal',
-              style: '',
-              text: 'This is 4th post of this feed.',
-              type: 'text',
-              version: 1,
-            },
-          ],
-          direction: 'ltr',
-          format: '',
-          indent: 0,
-          type: 'paragraph',
-          version: 1,
-        },
-      ],
-      direction: 'ltr',
-      format: '',
-      indent: 0,
-      type: 'root',
-      version: 1,
-    },
-  },
-  {
-    root: {
-      children: [
-        {
-          children: [
-            {
-              detail: 0,
-              format: 0,
-              mode: 'normal',
-              style: '',
-              text: 'This is 5th post of this feed.',
-              type: 'text',
-              version: 1,
-            },
-          ],
-          direction: 'ltr',
-          format: '',
-          indent: 0,
-          type: 'paragraph',
-          version: 1,
-        },
-      ],
-      direction: 'ltr',
-      format: '',
-      indent: 0,
-      type: 'root',
-      version: 1,
-    },
-  },
-  {
-    root: {
-      children: [
-        {
-          children: [
-            {
-              detail: 0,
-              format: 0,
-              mode: 'normal',
-              style: '',
-              text: 'This is 6th post of this feed.',
-              type: 'text',
-              version: 1,
-            },
-          ],
-          direction: 'ltr',
-          format: '',
-          indent: 0,
-          type: 'paragraph',
-          version: 1,
-        },
-      ],
-      direction: 'ltr',
-      format: '',
-      indent: 0,
-      type: 'root',
-      version: 1,
-    },
-  },
-  {
-    root: {
-      children: [
-        {
-          children: [
-            {
-              detail: 0,
-              format: 0,
-              mode: 'normal',
-              style: '',
-              text: 'This is 7th post of this feed.',
-              type: 'text',
-              version: 1,
-            },
-          ],
-          direction: 'ltr',
-          format: '',
-          indent: 0,
-          type: 'paragraph',
-          version: 1,
-        },
-      ],
-      direction: 'ltr',
-      format: '',
-      indent: 0,
-      type: 'root',
-      version: 1,
-    },
-  },
-  {
-    root: {
-      children: [
-        {
-          children: [
-            {
-              detail: 0,
-              format: 0,
-              mode: 'normal',
-              style: '',
-              text: 'This is 8th post of this feed.',
-              type: 'text',
-              version: 1,
-            },
-          ],
-          direction: 'ltr',
-          format: '',
-          indent: 0,
-          type: 'paragraph',
-          version: 1,
-        },
-      ],
-      direction: 'ltr',
-      format: '',
-      indent: 0,
-      type: 'root',
-      version: 1,
-    },
-  },
-  {
-    root: {
-      children: [
-        {
-          children: [
-            {
-              detail: 0,
-              format: 0,
-              mode: 'normal',
-              style: '',
-              text: 'This is 9th post of this feed.',
-              type: 'text',
-              version: 1,
-            },
-          ],
-          direction: 'ltr',
-          format: '',
-          indent: 0,
-          type: 'paragraph',
-          version: 1,
-        },
-      ],
-      direction: 'ltr',
-      format: '',
-      indent: 0,
-      type: 'root',
-      version: 1,
-    },
-  },
-  {
-    root: {
-      children: [
-        {
-          children: [
-            {
-              detail: 0,
-              format: 0,
-              mode: 'normal',
-              style: '',
-              text: 'This is 10th post of this feed.',
-              type: 'text',
-              version: 1,
-            },
-          ],
-          direction: 'ltr',
-          format: '',
-          indent: 0,
-          type: 'paragraph',
-          version: 1,
-        },
-      ],
-      direction: 'ltr',
-      format: '',
-      indent: 0,
-      type: 'root',
-      version: 1,
-    },
-  },
+  // {
+  //   root: {
+  //     children: [
+  //       {
+  //         children: [
+  //           {
+  //             detail: 0,
+  //             format: 0,
+  //             mode: 'normal',
+  //             style: '',
+  //             text: 'This is third post of this feed.',
+  //             type: 'text',
+  //             version: 1,
+  //           },
+  //         ],
+  //         direction: 'ltr',
+  //         format: '',
+  //         indent: 0,
+  //         type: 'paragraph',
+  //         version: 1,
+  //       },
+  //     ],
+  //     direction: 'ltr',
+  //     format: '',
+  //     indent: 0,
+  //     type: 'root',
+  //     version: 1,
+  //   },
+  // },
+  // {
+  //   root: {
+  //     children: [
+  //       {
+  //         children: [
+  //           {
+  //             detail: 0,
+  //             format: 0,
+  //             mode: 'normal',
+  //             style: '',
+  //             text: 'This is 4th post of this feed.',
+  //             type: 'text',
+  //             version: 1,
+  //           },
+  //         ],
+  //         direction: 'ltr',
+  //         format: '',
+  //         indent: 0,
+  //         type: 'paragraph',
+  //         version: 1,
+  //       },
+  //     ],
+  //     direction: 'ltr',
+  //     format: '',
+  //     indent: 0,
+  //     type: 'root',
+  //     version: 1,
+  //   },
+  // },
+  // {
+  //   root: {
+  //     children: [
+  //       {
+  //         children: [
+  //           {
+  //             detail: 0,
+  //             format: 0,
+  //             mode: 'normal',
+  //             style: '',
+  //             text: 'This is 5th post of this feed.',
+  //             type: 'text',
+  //             version: 1,
+  //           },
+  //         ],
+  //         direction: 'ltr',
+  //         format: '',
+  //         indent: 0,
+  //         type: 'paragraph',
+  //         version: 1,
+  //       },
+  //     ],
+  //     direction: 'ltr',
+  //     format: '',
+  //     indent: 0,
+  //     type: 'root',
+  //     version: 1,
+  //   },
+  // },
+  // {
+  //   root: {
+  //     children: [
+  //       {
+  //         children: [
+  //           {
+  //             detail: 0,
+  //             format: 0,
+  //             mode: 'normal',
+  //             style: '',
+  //             text: 'This is 6th post of this feed.',
+  //             type: 'text',
+  //             version: 1,
+  //           },
+  //         ],
+  //         direction: 'ltr',
+  //         format: '',
+  //         indent: 0,
+  //         type: 'paragraph',
+  //         version: 1,
+  //       },
+  //     ],
+  //     direction: 'ltr',
+  //     format: '',
+  //     indent: 0,
+  //     type: 'root',
+  //     version: 1,
+  //   },
+  // },
+  // {
+  //   root: {
+  //     children: [
+  //       {
+  //         children: [
+  //           {
+  //             detail: 0,
+  //             format: 0,
+  //             mode: 'normal',
+  //             style: '',
+  //             text: 'This is 7th post of this feed.',
+  //             type: 'text',
+  //             version: 1,
+  //           },
+  //         ],
+  //         direction: 'ltr',
+  //         format: '',
+  //         indent: 0,
+  //         type: 'paragraph',
+  //         version: 1,
+  //       },
+  //     ],
+  //     direction: 'ltr',
+  //     format: '',
+  //     indent: 0,
+  //     type: 'root',
+  //     version: 1,
+  //   },
+  // },
+  // {
+  //   root: {
+  //     children: [
+  //       {
+  //         children: [
+  //           {
+  //             detail: 0,
+  //             format: 0,
+  //             mode: 'normal',
+  //             style: '',
+  //             text: 'This is 8th post of this feed.',
+  //             type: 'text',
+  //             version: 1,
+  //           },
+  //         ],
+  //         direction: 'ltr',
+  //         format: '',
+  //         indent: 0,
+  //         type: 'paragraph',
+  //         version: 1,
+  //       },
+  //     ],
+  //     direction: 'ltr',
+  //     format: '',
+  //     indent: 0,
+  //     type: 'root',
+  //     version: 1,
+  //   },
+  // },
+  // {
+  //   root: {
+  //     children: [
+  //       {
+  //         children: [
+  //           {
+  //             detail: 0,
+  //             format: 0,
+  //             mode: 'normal',
+  //             style: '',
+  //             text: 'This is 9th post of this feed.',
+  //             type: 'text',
+  //             version: 1,
+  //           },
+  //         ],
+  //         direction: 'ltr',
+  //         format: '',
+  //         indent: 0,
+  //         type: 'paragraph',
+  //         version: 1,
+  //       },
+  //     ],
+  //     direction: 'ltr',
+  //     format: '',
+  //     indent: 0,
+  //     type: 'root',
+  //     version: 1,
+  //   },
+  // },
+  // {
+  //   root: {
+  //     children: [
+  //       {
+  //         children: [
+  //           {
+  //             detail: 0,
+  //             format: 0,
+  //             mode: 'normal',
+  //             style: '',
+  //             text: 'This is 10th post of this feed.',
+  //             type: 'text',
+  //             version: 1,
+  //           },
+  //         ],
+  //         direction: 'ltr',
+  //         format: '',
+  //         indent: 0,
+  //         type: 'paragraph',
+  //         version: 1,
+  //       },
+  //     ],
+  //     direction: 'ltr',
+  //     format: '',
+  //     indent: 0,
+  //     type: 'root',
+  //     version: 1,
+  //   },
+  // },
 ];
 
 const RAW_DUMMY_JSON_TO_SEND_TO_WEB_VIEW = {
@@ -463,4 +504,5 @@ const RAW_DUMMY_JSON_TO_SEND_TO_WEB_VIEW = {
   },
 };
 
-const DUMMY_STRINGIFIED = `{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"This is Sasikant. I’m a JS Developer at Torum.","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`;
+const DUMMY_STRINGIFIED =
+  '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"This is Sasikant. I’m a JS Developer at Torum.","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}';
